46f02c42994adfe672f345e670daa98e90d4d68e
Esmond Wong <ewong@hk.palo-it.com>
2021-07-12 14:52:10 +0800

    Restructure l10n

--- lib/posts/view/post_page.dart
+++ lib/posts/view/post_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:post_bloc_app/l10n/gen/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:post_bloc_app/posts/posts.dart';
 import 'package:post_repository/post_repository.dart';
 
